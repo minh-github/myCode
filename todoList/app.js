5db14ea5264fea9ed11a86b4d06604658f70006b
Phạm Quang Minh <phamminh@Admins-MacBook-Pro.local>
2022-03-23 00:21:15 +0700

    todo list

---
+++ todoList/app.js
@@ -0,0 +1,41 @@
+
+let button = document.querySelector('.button')
+
+document.querySelector('#myText').addEventListener('keypress',function(e){
+    if (e.key === 'Enter') {
+        myFuntion()
+      }
+})
+
+function myFuntion(){
+    let x = document.querySelector('#myText')
+    if(x.value.length == 0){
+        alert('bạn chưa nhập việc cần làm')
+    }
+    else{
+        Render(x.value)
+    }
+}
+
+function Render(x){
+    document.querySelector('.lists ul').innerHTML +=`
+        <li class="list">${x}<div class="close"><i class="fa-solid fa-xmark times"></i></div></li>
+    `
+    Clear()
+    Remove()
+}
+
+function Clear(){
+    document.querySelector('#myText').value = ''
+}
+
+function Remove(){
+    let Delete = document.querySelectorAll('.close')
+    let option = 'bạn có chắc chắn xóa'
+    for (const item of Delete) {
+        item.addEventListener('click',function(){
+            if(confirm(option))
+            item.parentNode.parentNode.removeChild(item.parentNode)
+        })
+    }
+}
